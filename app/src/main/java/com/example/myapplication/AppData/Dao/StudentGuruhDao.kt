73d1938d90a3e72d5bd5163dc73796cf548e2d49
Shohrux <shoxruxsharipov1105@gmail.com>
2023-05-17 06:11:46 +0500

    second commit

--- app/src/main/java/com/example/myapplication/AppData/Dao/StudentGuruhDao.kt
+++ app/src/main/java/com/example/myapplication/AppData/Dao/StudentGuruhDao.kt
@@ -2,10 +2,11 @@ package com.example.myapplication.AppData.Dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import com.example.myapplication.AppData.Entity.Userr
 
 @Dao
 interface StudentGuruhDao {
 
-//    @Query
-//    fun
+    @Query("select * from userr where guruh_id = :guruh ")
+    fun getGuruh(guruh:Int):List<Userr>
 }
